b8b171a10864b40c5b70b1a0f83a1da041633bc2
etterath <w.rapacz@protonmail.com>
2024-09-26 00:45:33 +0200

    feat: langchain orchestrator

---
+++ langchain_orchestrator.py
@@ -0,0 +1,23 @@
+from langchain.schema import Document
+from langchain.sql_database import SQLDatabase
+from langchain.chains import SQLDatabaseChain
+
+class LangChainOrchestrator:
+    def __init__(self, db_connector, vector_store):
+        self.db_connector = db_connector
+        self.vector_store = vector_store
+        self.sql_database = SQLDatabase(db_connector.engine)
+        self.sql_chain = None
+
+    def initialize_chain(self, llm):
+        self.sql_chain = SQLDatabaseChain.from_llm(llm, self.sql_database, verbose=True)
+
+    def add_schema_to_vector_store(self):
+        schema = self.sql_database.get_table_info()
+        self.vector_store.add_texts([schema])
+
+    def query(self, user_input):
+        relevant_docs = self.vector_store.similarity_search(user_input)
+        context = "\n".join([doc.page_content for doc in relevant_docs])
+        full_query = f"Context: {context}\nQuestion: {user_input}"
+        return self.sql_chain.run(full_query)
